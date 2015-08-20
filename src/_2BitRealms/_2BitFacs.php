5cfd8c24813262cc37d6c9b063a80b9d8a7b695c
DevBrad <devbrad2001@gmail.com>
2015-08-20 11:08:57 +0100

    Create _2BitFacs.php

---
+++ src/_2BitRealms/_2BitFacs.php
@@ -0,0 +1,24 @@
+php?>
+
+ 
+namespace _2BitRealms; 
+ 
+use pocketmine\command\Command; 
+use pocketmine\command\CommandSender; 
+use pocketmine\command\ConsoleCommandSender; 
+
+use pocketmine\plugin\PluginBase; 
+ 
+/* 
+ ____           ____  _ _     ____            _                
+|___ \         | __ )(_) |_  |  _ \ ___  __ _| |_ __ ___  ___  
+  __) |  ____  |  _ \| | __| | |_) / _ \/ _` | | '_ ` _ \/ __| 
+ / __/  |____| | |_) | | |_  |  _ <  __/ (_| | | | | | | \__ \ 
+|_____|        |____/|_|\__| |_| \_\___|\__,_|_|_| |_| |_|___/ 
+                    
+Plugin developed and published by 2-Bit Realms Dev Team 
+Developed as part of 2-Bit Realms, the ultimate, micro-host for Minecraft : Pocket Edition 
+This project is licensed under the terms of the GNU General Public License version 3. 
+28 2-Bit Realms by DevBrad (Kik: @xXDevBradXx) 
+29                      
+30 */ 
