1ba747c44d42d72235b479f033f36edfae0c3303
RandomAltThing <goldowl37@gmail.com>
2015-09-02 12:46:49 +0100

    Started SubCommands (Untested)

---
+++ src/_2BitRealms/command/SubCommand.php
@@ -0,0 +1,50 @@
+<?php
+namespace _2BitRealms\command;
+use _2BitRealms\_2BitFacs;
+use pocketmine\command\CommandSender;
+abstract class SubCommand{
+	
+    /** @var _2BitFacs */
+    private $plugin;
+    /**
+     * @param _2BitFacs $plugin
+     */
+    public function __construct(MyPlot $plugin){
+        $this->plugin = $plugin;
+    }
+    /**
+     * @return _2BitFacs
+     */
+    public final function getPlugin(){
+        return $this->plugin;
+    }
+    /**
+     * @param CommandSender $sender
+     * @return bool
+     */
+    public abstract function canUse(CommandSender $sender);
+    /**
+     * @return string
+     */
+    public abstract function getUsage();
+    /**
+     * @return string
+     */
+    public abstract function getName();
+    /**
+     * @return string
+     */
+    public abstract function getDescription();
+    /**
+     * @return string[]
+     */
+    public abstract function getAliases();
+    /**
+     * @param CommandSender $sender
+     * @param string[] $args
+     * @return bool
+     */
+    public abstract function execute(CommandSender $sender, array $args);
+	
+	}
+	
---
+++ src/_2BitRealms/command/SubCommandMap.php
@@ -0,0 +1,54 @@
+<?php
+namespace _2BitRealms\command;
+use pocketmine\command\PluginCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use _2BitRealms\_2BitFacs;
+class SubCommandMap extends PluginCommand {
+	
+    private $subCommands = [];
+    /* @var SubCommand[] */
+    private $commandObjects = [];
+	
+    public function __construct(_2BitFacs $plugin) {
+        parent::__construct("f", $plugin);
+        $this->setAliases(["faction"]);
+        $this->setPermission("2bitfacs.vommand.f");
+        $this->setDescription("Main command for _2BitFacs!");
+        
+    }
+	
+    private function loadSubCommand(Subcommand $command) {
+        $this->commandObjects[] = $command;
+        $commandId = count($this->commandObjects) - 1;
+        $this->subCommands[$command->getName()] = $commandId;
+        foreach ($command->getAliases() as $alias) {
+            $this->subCommands[$alias] = $commandId;
+        }
+    }
+	
+    public function execute(CommandSender $sender, $alias, array $args) {
+		if (!isset($args[0])) {
+            return $this->sendHelp($sender);
+        }
+        $subCommand = strtolower(array_shift($args));
+        if (!isset($this->subCommands[$subCommand])) {
+            return $this->sendHelp($sender);
+        }
+        $command = $this->commandObjects[$this->subCommands[$subCommand]];
+        $canUse = $command->canUse($sender);
+        if ($canUse) {
+            if (!$command->execute($sender, $args)) {
+                $sender->sendMessage(TextFormat::YELLOW."Usage: /f " . $command->getName() . " " . $command->getUsage());
+            }
+        } elseif (!($sender instanceof Player)) {
+            $sender->sendMessage(TextFormat::RED . "Please run this command in-game.");
+        } else {
+            $sender->sendMessage(TextFormat::RED. "You do not have permissions to run this command");
+        }
+        return true;
+	}
+	
+}
+	
