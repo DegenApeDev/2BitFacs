032d5c1fdc8370fa01aaa5be4e5f7282a8fef230
Angelo Vidrio <omgk45@gmail.com>
2015-09-04 12:14:39 -0700

    Update Faction.php
    I was working on this at school, I may have mistakes. Do not edit any of the code I wrote right now, I will have to finish when I get home. Thanks!

--- src/_2BitRealms/faction/Faction.php
+++ src/_2BitRealms/faction/Faction.php
@@ -1,12 +1,17 @@
 <?php
 namespace _2BitRealms\faction;
+use _2BitRealms\_2BitFacs;
 use pocketmine\utils\TextFormat as Colour;
+use pocketmine\level\Position;
 use pocketmine\Server;
 class Faction {
 	
 	private $name;
 	private $description;
 	private $leader;
+	private $players;
+	private $plugin;
+	private $home
 	
 	public function getName(){
 		return $this->name;
@@ -16,13 +21,35 @@ class Faction {
 		return $this->description;
 	}
 	
-	public function __construct($name, $leader){
+	public function getPlayers() {
+		return $this->players;
+	}
+	
+	public function getHome() {
+		return $this->home;	
+	}
+	
+	public function __construct(Main $plugin, Player $leader, $name, Position $home = null){
+		$this->plugin = $plugin;
 		$this->name = $name;
 		$this->description = "Default description :(";
 		$this->leader = $leader;
-		
+		$this->home = $home;
+		$this->players = array("Leader" => $leader->getDisplayName, "Mods" => "", "Members" => "";  //Can be used for /f who & getting the leader.
 		$this->broacastAll(Colour::WHITE. $leader->getDisplayName(). Colour::YELLOW. " created the faction ". Colour::WHITE. $name);
 		$leader->sendMessage(Colour::YELLOW. "Faction succesfully created!");
+		$this->storeFac();
+	}
+	
+	public function storeFac() {
+		$db = $this->plugin->db;
+		$sql = $db->prepare("INSERT INTO factions (name, description, players, home) VALUES (:name, :description, :players, :home);");
+		$sql->bindValue(":name", $this->name);
+		$sql->bindValue(":description", $this->description);
+		$sql->bindValue(":players", $this->players);
+		$sql->bindValue(":home", $this->home);
+		$result = $sql->execute();
+		return;
 	}
 	
 	public function broadcastAll($message){
