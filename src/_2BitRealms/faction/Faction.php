6a78e66227246ff4cdb4d55c019a8281eded6824
Angelo Vidrio <omgk45@gmail.com>
2015-09-15 12:05:15 -0700

    Update Faction.php

--- src/_2BitRealms/faction/Faction.php
+++ src/_2BitRealms/faction/Faction.php
@@ -9,9 +9,9 @@ class Faction {
 	private $name;
 	private $description;
 	private $leader;
-	private $players;
 	private $plugin;
-	private $home
+	private $mods;
+	private $members;
 	
 	public function getName(){
 		return $this->name;
@@ -21,21 +21,50 @@ class Faction {
 		return $this->description;
 	}
 	
-	public function getPlayers() {
-		return $this->players;
+	public function getLeader() {
+		return $this->leader;
 	}
 	
-	public function getHome() {
-		return $this->home;	
+	public function getMods() {
+		return $this->arrayString($this->mods);
+	}
+	
+	public function getMembers() {
+		return $this->arrayString($this->members);
+	}
+	
+	//If a Moderaotr, player will be demoted to Member. If player is a Members, that player will be kicked from the faction.
+	public function demote($player) {
+		$mods = $this->getMods();
+		$members = $this->getMembers();
+		foreach($mods as $key => $mod) {
+			if($mod === $player) {
+				unset($mods[$key]);
+				array_push($members, $mod);
+				$this->mods = implode(",", $mods);
+				$this->members = implode(",", $members);
+				$this->storeFac();
+				return;
+			}
+		}
+		foreach($this->members as $key => $member) {
+			if($member === $player) {
+				unset($members[$key]);
+				$this->members = implode(",", $members);
+				$this->storeFac();
+				return;
+			}
+		}
 	}
 	
    //TO BE USED FOR INTERNAL USE ONLY
-	public function __construct(Main $plugin, Player $leader, $name){
+	public function __construct(Main $plugin, Player $leader, $name, $mods = null, $members = null){
 		$this->plugin = $plugin;
 		$this->name = $name;
 		$this->description = "Default description :(";
 		$this->leader = $leader;
-		$this->players = array("Leader" => $leader->getDisplayName(), "Mods" => "", "Members" => "";  //Can be used for /f who & getting the leader.
+		$this->mods = $mods;
+		$this->members = $members;
 		
 		//Send the faction created message to everyone but the leader
 		$this->broacastAll(Colour::WHITE. $leader->getDisplayName(). Colour::YELLOW. " created the faction ". Colour::WHITE. $name);
@@ -47,11 +76,12 @@ class Faction {
 	
 	public function storeFac() {
 		$db = $this->plugin->db;
-		$sql = $db->prepare("INSERT INTO factions (name, description, players, home) VALUES (:name, :description, :players, :home);");
+		$sql = $db->prepare("INSERT INTO factions (name, description, leader, mods, members) VALUES (:name, :description, :leader, :mods, :members);");
 		$sql->bindValue(":name", $this->name);
 		$sql->bindValue(":description", $this->description);
-		$sql->bindValue(":players", $this->players);
-		$sql->bindValue(":home", $this->home);
+		$sql->bindValue(":leader", $this->leader);
+		$sql->bindValue(":mods", $this->mods);
+		$sql->bindValue(":members", $this->members);
 		$result = $sql->execute();
 		return;
 	}
@@ -61,6 +91,16 @@ class Faction {
 			if(!$p === $this->leader){
 				$p->sendMessage($message);
 			}
+		}
+	}
+	
+	//Turns a string into an array
+	public function arrayString($string) {
+		if($string === null) {
+			return null;
+		} else {
+			$array = explode(",", $string);
+			return $array;
 		}
 	}
 	
